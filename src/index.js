e9599334e021abc0622c714ec8a9019f382d03c1
Asuka-Hongo <75460862+araaiko@users.noreply.github.com>
2021-07-19 17:11:43 +0900

    CSS作成、タスクの追加機能（テキスト）作成

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.className = "todo-item";
+
+  // divタグ生成
+  const div = document.createElement("div");
+  div.className = "todo-item-wrap";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.className = "todo-text";
+  // pタグの中にinputTextの値を出力
+  p.innerText = inputText;
+
+  // liタグの中に各子要素を設定
+  li.appendChild(div);
+  div.appendChild(p);
+
+  // 未完了リストに追加、ただし、inputに入力されている場合のみ
+  inputText !== ""
+    ? document.getElementById("incomplete-list").appendChild(li)
+    : alert("TODOを入力してください");
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
